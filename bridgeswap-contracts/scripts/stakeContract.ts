d822373eeda2700d611b93777a104398cf6334bb
Akul Penugonda <akulvp@gmail.com>
2021-12-16 11:48:27 -0500

    Update setup script

--- bridgeswap-contracts/scripts/stakeContract.ts
+++ bridgeswap-contracts/scripts/stakeContract.ts
@@ -3,13 +3,16 @@ import { WS_RPC } from '@vite/vitejs-ws';
 // @ts-ignore
 import { HTTP_RPC } from '@vite/vitejs-http';
 import { wallet, accountBlock, ViteAPI, constant } from '@vite/vitejs';
+import { deploy, awaitDeployConfirmed, DeployedContract } from '../src/contract'
+import { CompileResult } from '../src/solppc'
+import { compile } from '../src/compile'
 
 const { getWallet } = wallet;
 const { Vite_TokenId } = constant;
 
 const mnemonic = process.env.MNEMONIC;
 if (!mnemonic) {
-    process.exit(1)
+  process.exit(1)
 }
 const mywallet = getWallet(mnemonic);
 const { privateKey, address } = mywallet.deriveAddress(0);
@@ -18,17 +21,10 @@ console.log("Here");
 
 const { createAccountBlock, utils } = accountBlock;
 
-const myAccountBlock = createAccountBlock('stakeForQuota', {
-    address,
-    beneficiaryAddress: "vite_016ff5fd6edb227deffca52044040f6a47b6ebfd01753570f0",
-    //amount: '1000000000000000000000' // The minimum staking amount is 134 VITE
-    amount: "5134000000000000000000"
-});
 
-console.log(myAccountBlock)
 
 function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 
@@ -37,27 +33,65 @@ console.log(wsrpc)
 const provider = new ViteAPI(wsrpc, () => { console.log("connect") });
 
 async function awaitReceived(hash: string) {
-    let outputblock = undefined;
-    while (!outputblock) {
-        const thisBlock = await provider.request('ledger_getAccountBlockByHash', hash);
-        if (!thisBlock?.receiveBlockHash) {
-            await sleep(1000);
-            continue
-        }
-        outputblock = thisBlock
+  let outputblock = undefined;
+  while (!outputblock) {
+    const thisBlock = await provider.request('ledger_getAccountBlockByHash', hash);
+    if (!thisBlock?.receiveBlockHash) {
+      await sleep(1000);
+      continue
     }
+    outputblock = thisBlock
+  }
+  return outputblock
 }
 
+
+async function _deployContract() {
+  const compileResult: CompileResult = await compile("Bridgeswap.solpp")
+  //  console.log("CompileResult", compileResult)
+  const sendBlock = await deploy(
+    provider,
+    address,
+    privateKey,
+    compileResult.abis[0],
+    compileResult.byteCodes[0],
+    {
+      responseLatency: 15
+    }
+  )
+
+  console.log("Sent")
+  const receiveblock = await awaitReceived(sendBlock.hash!);
+  console.log("Received");
+  const contract = new DeployedContract(
+    provider,
+    sendBlock.toAddress!,
+    compileResult.abis[0],
+    compileResult.offChainCodes[0]
+  )
+  console.log("Contracthere", contract)
+  return contract
+}
 async function mintToken() {
-    console.log("Provider created", provider);
-    myAccountBlock.setProvider(provider).setPrivateKey(privateKey);
-    await myAccountBlock.autoSetPreviousAccountBlock();
-    console.log("Here");
-    const result = await myAccountBlock.sign().send();
-    console.log('send success', result);
-    //    await provider.request("miner_mine");
-    await awaitReceived(myAccountBlock.hash!);
-    return 0;
+  console.log("Provider created", provider);
+  const thiscontract: DeployedContract = await _deployContract();
+  console.log("Contract:", thiscontract)
+  const myAccountBlock = createAccountBlock('stakeForQuota', {
+    address,
+    beneficiaryAddress: thiscontract._address,
+    //amount: '1000000000000000000000' // The minimum staking amount is 134 VITE
+    amount: "5134000000000000000000"
+  });
+  myAccountBlock.setProvider(provider).setPrivateKey(privateKey);
+
+  await myAccountBlock.autoSetPreviousAccountBlock();
+  console.log("Here");
+  const result = await myAccountBlock.sign().send();
+  console.log('send success', result);
+  //    await provider.request("miner_mine");
+  await awaitReceived(myAccountBlock.hash!);
+  //    const initializeBlock = await thiscontract.call(address, privateKey,
+  return 0;
 }
 
 mintToken().catch((alert) => { console.log(alert); }).then(() => { console.log("Done") });
